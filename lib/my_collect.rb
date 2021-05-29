b154045b6bb483b24e55b46c91c90787e78b013d
Noah  Klem <klem.noah@gmail.com>
2021-05-29 03:22:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  #just hand built the collect method to understand what happens
+  #behind the scenes
+  i = 0
+  new_collection = []
+  
+  while i < array.length
+    new_collection<< yield(array[i])
+    i +=1
+  end
+  new_collection
+end
+    
