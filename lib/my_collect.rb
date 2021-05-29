bfdbe616f1203648d97d85e05ded736297668ba9
Noah  Klem <klem.noah@gmail.com>
2021-05-29 03:18:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,11 @@
-def my_collect(collection)
+def my_collect(array)
+  i = 0
+  new_collection = []
   
-
+  while i < array.length
+    new.collection<< yield(array[i])
+    i +=1
+  end
+  new_collection
+end
+    
